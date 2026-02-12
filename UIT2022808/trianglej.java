ac7bcc53166d51eb2a9ab0f65cc00282a52ba4ec
annanyagali <annanya.gali@cumminscollege.in>
2026-02-12 10:07:29 +0530

    Added trianglej and triangletest in UIT2022808 folder

---
+++ UIT2022808/trianglej.java
@@ -0,0 +1,7 @@
+package UIT2022808;
+
+public class trianglej {
+    public static boolean isTriangle(double l, double b, double h) {
+        return (l + b > h) && (l + h > b) && (b + h > l);
+    }
+}
